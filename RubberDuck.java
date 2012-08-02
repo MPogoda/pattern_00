f8b528ee3a843e2525e7b92501219dc4dc4d6fb8
MPogoda <do0dlez1991@gmail.com>
2012-08-02 15:14:17 +0300

    trying to fix FLYING RUBBER DUCKS with overriding

--- RubberDuck.java
+++ RubberDuck.java
@@ -5,4 +5,14 @@ public class RubberDuck extends Duck {
   public void display() {
     System.out.println("I'm rubber duck!");
   }
+
+  @Override
+  public void fly() {
+    // Do nothing : rubber ducks can't fly :<
+  }
+
+  @Override
+  public void quack() {
+    System.out.println("Squeeek!");
+  }
 }
